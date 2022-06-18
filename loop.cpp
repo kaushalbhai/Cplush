64e7924b70a9daf98a7fdfdc6e24ad5f1389ea6e
kaushalbhai <94095824+kaushalbhai@users.noreply.github.com>
2022-06-19 00:10:55 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,109 @@
+// For loop
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for (int  i = 0; i <=7; i++)
+//     {
+//         cout<<i<<" ";
+//         /* code */
+//     }
+//     return 0;
+    
+// }
+
+// While loop 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i =1;
+//     while (i<=8)
+//     {
+//        cout<<i<<" ";
+//        i++;
+//     }
+//     return 0;
+    
+// }
+
+// do......while loop
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i = 1;
+//     do
+//     {
+//         cout<<i<<" ";
+//         i++;
+//     } while (i<=5);
+//     return 0;
+// }
+
+
+// Jumps in loops.....
+//  continue statement..............
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for (int i = 0;  i <= 10; i++)
+//     {
+//         if (i==5)
+//         {
+//             continue;
+//         }
+//         cout<<i<<"\n";
+        
+//     }
+    
+// }
+
+// Break statement............
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for (int i = 0; i <=12; i++)
+//     {
+//         if (i==6)
+//         {
+//             break;
+//         }
+//         cout<<i<<"\n";
+        
+//     }
+    
+// }d 
+
+// Switch case Staement.......
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int day =6;
+   
+//     switch (day)
+//     {
+//     case 1:
+//         cout<<"Monday";
+//         break;
+//     case 2:
+//         cout<<"Tuesday";
+//         break;
+//     case 3:
+//         cout<<"Wednesday";
+//         break;
+//     case 4:
+//         cout<<"Thursday";
+//         break;
+//     case 5:
+//         cout<<"Friday";
+//         break;
+//     case 6:
+//         cout<<"Saturday";
+//         break;
+//     default:
+//         cout<<"Sorry this is not in a list";
+//         break;
+//     }
+// }
+
+
